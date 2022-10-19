3419b3c78955b7a8cb1da4ec1296ae58b1bc0952
박은빈 <joypeb@naver.com>
2022-10-19 10:51:44 +0900

    stack push, pop구현, 테스트 작성

---
+++ src/main/java/algorithm/Bracket.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Bracket {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+
+        String brackets = "";˚
+    }
+}
---
+++ src/main/java/algorithm/Stack.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack {
+    private int[] arr = new int[10000];
+    private List<Integer> arl = new ArrayList<>();
+    private int pointer = 0;
+
+    public Stack() {
+    }
+
+    public Stack(int size) {
+        this.arr = new int[size];
+    }
+
+    public void push(int value) {
+        this.arr[this.pointer] = value;
+        pointer++;
+    }
+
+    public int pop() {
+        int value = this.arr[this.pointer-1];
+        this.pointer--;
+        return value;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+}
