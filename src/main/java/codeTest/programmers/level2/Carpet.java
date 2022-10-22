ce4ea02ed5b77ec903f3f942c3327e8d08015df1
박은빈 <joypeb@naver.com>
2022-10-22 21:19:49 +0900

    프로그래머스 카펫

---
+++ src/main/java/codeTest/programmers/level2/Carpet.java
@@ -0,0 +1,24 @@
+package codeTest.programmers.level2;
+
+public class Carpet {
+    //카펫
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        int sum = brown + yellow;
+        int h = 0;
+
+        for(int w=sum/2; w>2; w--){
+            if(sum%w == 0) {
+                h = sum/w;
+                if((w-2)*(h-2) == yellow) {
+                    answer[0] = w;
+                    answer[1] = h;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
