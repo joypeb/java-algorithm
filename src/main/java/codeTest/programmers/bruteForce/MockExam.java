df1f62472f70328b22f3042f9318dd2393bcc68a
박은빈 <joypeb@naver.com>
2022-10-31 10:46:56 +0900

    모의고사.2

--- src/main/java/codeTest/programmers/bruteForce/MockExam.java
+++ src/main/java/codeTest/programmers/bruteForce/MockExam.java
@@ -1,37 +1,55 @@
 package codeTest.programmers.bruteForce;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MockExam {
     public int[] solution(int[] answers) {
 
-        int[] supo1 = {1,2,3,4,5};
-        int[] supo2 = {2,1,2,3,2,4,2,5};
-        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] supo1 = {1, 2, 3, 4, 5};
+        int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        int[] cnt = {0,0,0};
 
-        for(int i=0; i< supo3.length; i++) {
-            if(i< supo1.length) {
-                if(supo1[i] == answers[i]) {
-                    cnt1++;
-                }
+        for(int i=0; i<answers.length; i++) {
+            if(supo1[i%supo1.length] == answers[i]) {
+                cnt[0]++;
             }
-
-            if(i< supo2.length) {
-                if(supo2[i] == answers[i]) {
-                    cnt2++;
-                }
+            if(supo2[i%supo2.length] == answers[i]) {
+                cnt[1]++;
             }
-
-            if(supo3[i] == answers[i]) {
-                cnt3++;
+            if(supo2[i%supo2.length] == answers[i]) {
+                cnt[2]++;
             }
         }
 
+        int max = Math.max(Math.max(cnt[0], cnt[1]),cnt[2]);
 
+        ArrayList<Integer> maxList = new ArrayList<>();
+
+        for(int i=0; i<cnt.length; i++) {
+            if(cnt[i] == max) {
+                maxList.add(i+1);
+            }
+        }
+
+        int[] answer = new int[maxList.size()];
+        for(int i=0; i< answer.length; i++) {
+            answer[i] = maxList.get(i);
+        }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        MockExam mockExam = new MockExam();
+
+        int[] test = {1,2,3,4,5,1,2,3,4,5};
+
+        int[] reulst = mockExam.solution(test);
+
+        System.out.println(Arrays.toString(reulst));
     }
 }
