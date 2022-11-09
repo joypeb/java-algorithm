ebee12afc34869484f217b6106e53436abfb1a52
박은빈 <joypeb@naver.com>
2022-11-09 09:35:37 +0900

    선택정렬 완료

--- src/main/java/algorithm/sort/SelectionSort.java
+++ src/main/java/algorithm/sort/SelectionSort.java
@@ -1,19 +1,21 @@
 package algorithm.sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {2,7,4,9,10,223,111,23,3,39};
 
-        int min = arr[0];
-
-        for(int i=1; i< arr.length; i++) {
-            if(min > arr[i]) {
-                int swap = arr[i];
-                arr[i] = min;
-                min = swap;
+        for(int i=0; i< arr.length; i++) {
+            for(int j=i+1; j< arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int swap = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = swap;
+                }
             }
         }
 
-        System.out.println(min);
+        System.out.println(Arrays.toString(arr));
     }
 }
