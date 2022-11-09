15fff4710d917b9f74619ac5bdcf6ca4890bc96e
박은빈 <joypeb@naver.com>
2022-11-09 10:05:44 +0900

    선택정렬 인터페이스, 람다1

--- src/main/java/algorithm/sort/SelectionSort.java
+++ src/main/java/algorithm/sort/SelectionSort.java
@@ -3,12 +3,10 @@ package algorithm.sort;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] arr = {2,7,4,9,10,223,111,23,3,39};
-
+    public int[] solutionSort(int[] arr, Sort sort) {
         for(int i=0; i< arr.length; i++) {
             for(int j=i+1; j< arr.length; j++) {
-                if(arr[i] > arr[j]) {
+                if(sort.selectionSort(arr[i],arr[j])) {
                     int swap = arr[j];
                     arr[j] = arr[i];
                     arr[i] = swap;
@@ -16,6 +14,25 @@ public class SelectionSort {
             }
         }
 
-        System.out.println(Arrays.toString(arr));
+        return arr;
     }
+
+    public static void main(String[] args) {
+        int[] arr = {2,7,4,9,10,223,111,23,3,39};
+
+        Sort desc = (a,b) -> a < b;
+        Sort asc = (a,b) -> a > b;
+
+        SelectionSort selectionSort = new SelectionSort();
+
+        int[] descArr = selectionSort.solutionSort(arr,desc);
+        int[] ascArr = selectionSort.solutionSort(arr,asc);
+
+        System.out.println(Arrays.toString(descArr));
+        System.out.println(Arrays.toString(ascArr));
+    }
+}
+
+interface Sort {
+    boolean selectionSort(int a, int b);
 }
