c00b0dc8be837bbf9aae13ea57b1da544b352b82
박은빈 <joypeb@naver.com>
2022-11-10 10:35:21 +0900

    기능개발 완료

--- src/main/java/codeTest/programmers/level2/FunctionDevelopment.java
+++ src/main/java/codeTest/programmers/level2/FunctionDevelopment.java
@@ -1,12 +1,10 @@
 package codeTest.programmers.level2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FunctionDevelopment {
     //기능 개발
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
         int[] proDay = new int[progresses.length];
 
         for(int i=0; i<progresses.length; i++) {
@@ -14,36 +12,38 @@ public class FunctionDevelopment {
             proDay[i] = (int)Math.ceil(num/speeds[i]);
         }
 
-        int cnt = 0;
-        int maxPro = proDay[0];
-        list.add(1);
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        int max = proDay[0];
+        int cnt = 1;
 
         for(int i=1; i< proDay.length; i++) {
-            if(proDay[i] < maxPro) {
-                list.set(cnt, list.get(cnt) + 1);
-            } else {
-                cnt++;
-                list.add(1);
-                maxPro = proDay[i];
+            if(max >= proDay[i]) {
+                cnt = stack.pop()+1;
+                stack.push(cnt);
+            } else{
+                stack.push(1);
+                max = proDay[i];
             }
         }
 
-        int[] answer = new int[list.size()];
+        int[] answer = new int[stack.size()];
 
-        for(int i=0; i< list.size(); i++) {
-            answer[i] = list.get(i);
+        for(int i=stack.size()-1; i>=0; i--) {
+            answer[i] = stack.pop();
         }
 
         return answer;
     }
 
     public static void main(String[] args) {
-        int i = 100;
-        int j = 30;
-        float k = 30;
+        int[] pro = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1,1,1,1,1,1};
 
-        float jj = 70/k;
+        FunctionDevelopment functionDevelopment = new FunctionDevelopment();
 
-        System.out.println((int)Math.ceil(jj));
+        int[] result = functionDevelopment.solution(pro,speeds);
+        System.out.println(Arrays.toString(result));
     }
 }
