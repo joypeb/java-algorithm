1f989cce01e243659e4497315e2fb479e602c6c1
박은빈 <joypeb@naver.com>
2022-11-22 17:00:52 +0900

    PCCP 예산 성공

--- src/main/java/codeTest/programmers/level1/Budget.java
+++ src/main/java/codeTest/programmers/level1/Budget.java
@@ -12,14 +12,18 @@ public class Budget {
         Arrays.sort(d);
 
         for(int x : d) {
+            //합과 answer를 구하는 코드가 if문보다 앞에 와야한다
+            //뒤에 둘경우 answer--가 먼저 실행되서 제대로 동작을 안할 수도 있어 테스트케이스3에서 오류가 난다
+            sum+=x;
+            answer++;
             if(sum == budget) {
+                //같을 경우 최대예산이기때문에 그대로 answer리턴
                 break;
             } else if(sum > budget) {
+                //클경우 예산초과이기때문에 하나를 제거하고 리턴
                 answer--;
                 break;
             }
-            sum+=x;
-            answer++;
         }
 
         return answer;
