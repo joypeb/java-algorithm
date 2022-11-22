2373b0a7a79ce1ce7f4c95e540bb1a575cbe4668
박은빈 <joypeb@naver.com>
2022-11-22 21:31:05 +0900

    PCCP jadencase 3 to char array

--- src/main/java/pccp/JadenCase.java
+++ src/main/java/pccp/JadenCase.java
@@ -3,30 +3,29 @@ package pccp;
 public class JadenCase {
     public String solution(String s) {
         String answer = "";
-        String[] arr = s.split(" ");
+        char[] arr = s.toLowerCase().toCharArray();
 
-        for(int i=0; i<arr.length; i++) {
-            char first = arr[i].charAt(0);
-            if(48 <= first && first <= 57) {
-                arr[i] = arr[i].toLowerCase();
-            } else {
-                String words = arr[i].substring(1).toLowerCase();
-                if(97 <= first && first <= 122) {
-                    first-=32;
-                }
+        if(!(48 <= arr[0] && arr[0] <= 57)) {
+            arr[0] -= 32;
+        }
 
-                arr[i] = first + words;
+        answer += arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i-1] == 32 && !(48 <= arr[i] && arr[i] <= 57)) {
+                arr[i] -= 32;
             }
+            answer+=arr[i];
         }
 
-        for(String x : arr) {
-            answer += x + " ";
-        }
-
-        return answer.trim();
+        return answer;
     }
 
     public static void main(String[] args) {
+        JadenCase jadenCase = new JadenCase();
 
+        String s = "3people unFollowed me";
+
+        System.out.println(jadenCase.solution(s));
     }
 }
