1b638f3b7cc37b7504453cd2be1ca29d712449f7
박은빈 <joypeb@naver.com>
2022-11-22 18:14:36 +0900

    PCCP jadencase 2 런타임에러

--- src/main/java/pccp/JadenCase.java
+++ src/main/java/pccp/JadenCase.java
@@ -6,10 +6,24 @@ public class JadenCase {
         String[] arr = s.split(" ");
 
         for(int i=0; i<arr.length; i++) {
+            char first = arr[i].charAt(0);
+            if(48 <= first && first <= 57) {
+                arr[i] = arr[i].toLowerCase();
+            } else {
+                String words = arr[i].substring(1).toLowerCase();
+                if(97 <= first && first <= 122) {
+                    first-=32;
+                }
 
+                arr[i] = first + words;
+            }
         }
 
-        return answer;
+        for(String x : arr) {
+            answer += x + " ";
+        }
+
+        return answer.trim();
     }
 
     public static void main(String[] args) {
