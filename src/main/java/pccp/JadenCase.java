84c559f426def86fb9265848b14c56f2738e4f3e
박은빈 <joypeb@naver.com>
2022-11-22 18:05:22 +0900

    PCCP jadencase 1

---
+++ src/main/java/pccp/JadenCase.java
@@ -0,0 +1,18 @@
+package pccp;
+
+public class JadenCase {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+
+        for(int i=0; i<arr.length; i++) {
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
