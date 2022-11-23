fd53b01853078da95e0a2d73a095a69411296493
박은빈 <joypeb@naver.com>
2022-11-24 00:57:25 +0900

    PCCP 주식 가격 이중for문 성공

--- src/main/java/pccp/StockPrice.java
+++ src/main/java/pccp/StockPrice.java
@@ -8,30 +8,17 @@ public class StockPrice {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
 
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(prices[0]);
-        int answerTracker = 0;
-
-        for(int i=1; i<prices.length; i++) {
-            q.offer(prices[i]);
-
-            if(prices[i-1] > prices[i]) {
-               for(int j=answerTracker; j<i-1; j++) {
-                   answer[j] = q.size();
-                   q.poll();
+        for(int i=0; i< prices.length-1; i++) {
+            int cnt = 1;
+            inner:for(int j=i+1; j< prices.length-1; j++) {
+               if(prices[i] > prices[j]) {
+                   break inner;
+               } else {
+                   cnt++;
                }
-               answer[i-1] = 1;
-               q.poll();
-               answerTracker = i;
             }
+            answer[i] = cnt;
         }
-
-        for(int i=answerTracker; i< prices.length-1; i++) {
-            answer[i] = q.size();
-            q.poll();
-        }
-        answer[prices.length-2] = 1;
-        answer[prices.length-1] = 0;
 
         return answer;
     }
