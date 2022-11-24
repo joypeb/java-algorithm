8f743714d7563f4ac5f1e5073a716632d4601af6
박은빈 <joypeb@naver.com>
2022-11-24 10:59:52 +0900

    PCCP H-index 완료

--- src/main/java/pccp/Hindex.java
+++ src/main/java/pccp/Hindex.java
@@ -7,11 +7,23 @@ public class Hindex {
         int answer = 0;
 
         Arrays.sort(citations);
+        int num = citations[citations.length-1];
 
-        for(int i=0; i<citations.length; i++) {
-
-            if(citations.length-i >= citations[i]) {
-                return citations.length-i;
+        for(int i=num; i>=0; i--) {
+            int cnt = 0;
+            inner: for(int j= citations.length-1; j>=0; j--) {
+                if(i <= citations[j]) {
+                    cnt++;
+                } else {
+                    break inner;
+                }
+            }
+            if(cnt == i) {
+                answer = i;
+                break;
+            } else if(cnt >= i) {
+                answer = i;
+                break;
             }
         }
 
@@ -21,8 +33,10 @@ public class Hindex {
     public static void main(String[] args) {
         Hindex hindex = new Hindex();
 
-        int[] arr = {6, 5, 5, 5, 3, 2, 1, 0};
+        int[] arr = {0,1,2,3,5,5,5,6};
+        int[] arr2 =  {0,1,1};
 
-        System.out.println(hindex.solution(arr));
+        /*System.out.println(hindex.solution(arr));*/
+        System.out.println(hindex.solution(arr2));
     }
 }
