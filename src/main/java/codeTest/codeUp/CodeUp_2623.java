bc040efcb312055b78e4a31691adcd6bc17a2d78
박은빈 <joypeb@naver.com>
2022-11-25 09:21:20 +0900

    최대공약수 2

--- src/main/java/codeTest/codeUp/CodeUp_2623.java
+++ src/main/java/codeTest/codeUp/CodeUp_2623.java
@@ -1,7 +1,29 @@
 package codeTest.codeUp;
 
+import java.util.Scanner;
+
 public class CodeUp_2623 {
     //최대공약수 구하기
 
+    public static int gcd(int n, int m) {
+       if(n == m) {
+           return m;
+       }
 
+       return gcd(n-m,m);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int result = 0;
+
+        if(n > m) result = gcd(n,m);
+        else result = gcd(m,n);
+
+        System.out.println(result);
+    }
 }
