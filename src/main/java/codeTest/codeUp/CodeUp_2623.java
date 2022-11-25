6f7d8b4980b0fa70b6599fa30dfd1c7f9f67481b
박은빈 <joypeb@naver.com>
2022-11-25 09:25:50 +0900

    최대공약수 3

--- src/main/java/codeTest/codeUp/CodeUp_2623.java
+++ src/main/java/codeTest/codeUp/CodeUp_2623.java
@@ -6,11 +6,13 @@ public class CodeUp_2623 {
     //최대공약수 구하기
 
     public static int gcd(int n, int m) {
-       if(n == m) {
+       if(n > m) {
+           return gcd(n-m,m);
+       } else if (m > n) {
+           return gcd(m-n, n);
+       } else {
            return m;
        }
-
-       return gcd(n-m,m);
     }
 
     public static void main(String[] args) {
