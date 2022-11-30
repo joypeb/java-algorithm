61d90fe1043b67257c5831da2eeef9b0146e5cac
박은빈 <joypeb@naver.com>
2022-11-30 09:42:08 +0900

    두 노드간의 거리 2

--- src/main/java/codeTest/codeUp/CodeUp_1936.java
+++ src/main/java/codeTest/codeUp/CodeUp_1936.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class CodeUp_1936 {
     //두 노드간의 거리
     public static int tree(int start, int end, int cnt) {
-        if(start == end) return cnt;
-        else if(start == 1) return cnt;
-        else return tree(Math.floorDiv(start,2),end,cnt+1);
+        if(start < end)
+            return tree(start,Math.floorDiv(end,2), cnt+1);
+        else if (start > end)
+            return tree(Math.floorDiv(start,2),end, cnt+1);
+        else return cnt;
     }
 
     public static void main(String[] args) {
@@ -16,8 +18,7 @@ public class CodeUp_1936 {
         int start = sc.nextInt();
         int end = sc.nextInt();
 
-        int result1 = tree(start,end,0);
-        int result2 = tree(end, start, 0);
-        System.out.println(result1 + result2);
+        int result = tree(start,end, 0);
+        System.out.println(result);
     }
 }
