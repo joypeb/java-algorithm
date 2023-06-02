2b8b61c281d91c034070c753b258377747b0656d
박은빈 <joypeb@naver.com>
2023-06-02 17:03:49 +0900

    인프런 최대수입스케쥴

---
+++ src/main/java/inflearn/java_algorithm/Inf9_4BestIncomeSchedule.java
@@ -0,0 +1,76 @@
+package inflearn.java_algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Schedule implements Comparable<Schedule>{
+
+    int m,d;
+
+    public Schedule(int m, int d) {
+        this.m = m;
+        this.d = d;
+    }
+
+    @Override
+    public int compareTo(Schedule o) {
+        if(o.d == this.d) return o.m - this.m;
+        return o.d - this.d;
+    }
+}
+
+public class Inf9_4BestIncomeSchedule {
+    //최대 수입 스케쥴
+    /*
+    *
+    * 60 3
+    * 50 2
+    * 40 2
+    * 30 3
+    * 30 1
+    * 20 1
+    * */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int max = 0;
+
+        ArrayList<Schedule> arr = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            int m = sc.nextInt();
+            int d = sc.nextInt();
+
+            arr.add(new Schedule(m,d));
+            max = Math.max(max, d);
+        }
+
+        System.out.println(solution(arr, max));
+    }
+
+    public static int solution(ArrayList<Schedule> arr, int max) {
+        int result = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        Collections.sort(arr);
+
+        for(Schedule s : arr) {
+            if(s.d == max) {
+                pq.add(s.m);
+            } else {
+                result += pq.poll();
+                max--;
+                pq.add(s.m);
+            }
+        }
+
+        result += pq.poll();
+
+        return result;
+    }
+}
